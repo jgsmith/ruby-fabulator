312b69f3c3bd7873f723e7cad106c1486dd1c30a
James Smith <jgsmith@tamu.edu>
2010-12-08 16:52:37 +0000

    Fix attribute handling when expecting to use f:eval

--- lib/fabulator/lib/attribute.rb
+++ lib/fabulator/lib/attribute.rb
@@ -7,13 +7,18 @@ module Fabulator
       attribute :ns, :static => true, :inherited => true
       attribute :static, :static => true, :type => :boolean
       attribute :eval, :static => true, :type => :boolean
+      attribute :inherited, :static => true, :type => :boolean
 
       def is_static?
         @static
       end
 
       def value(context)
-        context.attribute(@ns, @name, { :static => @static, :eval => @eval })
+        v = context.attribute(@ns, @name, { :static => @static, :eval => @eval, :inherited => @inherited })
+        if @eval || !@static
+          v = context.root.anon_node(v, [ FAB_NS, 'expression' ])
+        end
+        v
       end
     end
   end
